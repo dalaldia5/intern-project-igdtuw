a7ac2195944ee3a4d02222d176a498332b6306ea
AnnuAnshu26 <annuanshu2005@gmail.com>
2025-08-02 21:21:24 +0530

    restructed file structure

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use('/api/teams', teamRoute); // <-- AND ADD THIS
 app.use('/api/tasks', taskRoute); // 👈 AND ADD THIS
 
 
-const PORT = 5000;
+const PORT = 5000;z
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
